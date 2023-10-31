22f707bcb4b9e6b88c4db4d4d3f4b4c9400c54da
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-10-31 20:27:25 -0300

    Closures in Effects

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import { memo, useCallback, useEffect } from "react";
+import { memo, useEffect } from "react";
 import { useState } from "react";
 import clickSound from "./ClickSound.m4a";
 
@@ -9,15 +9,6 @@ function Calculator({ workouts, allowSound }) {
   const [durationBreak, setDurationBreak] = useState(5);
 
   const [duration, setDuration] = useState(0);
-
-  // const playSound = useCallback(
-  //   function () {
-  //     if (!allowSound) return;
-  //     const sound = new Audio(clickSound);
-  //     sound.play();
-  //   },
-  //   [allowSound]
-  // );
 
   useEffect(() => {
     setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
@@ -36,7 +27,6 @@ function Calculator({ workouts, allowSound }) {
     [duration, allowSound]
   );
 
-  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
